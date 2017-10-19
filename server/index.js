1d7ef2029c8c9af7cb78d927b26ab83bae99fb14
Claire Johnson <c.eliseee@gmail.com>
2017-10-19 16:14:35 -0700

    (feat) setting up

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+// const router = require('./routes');
+const path = require('path');
+// const sock = require('./sock')
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '/../public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', (req, res) => {
+  res.send('hello world');
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {});
+
+module.exports = app;
