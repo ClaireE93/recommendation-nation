26e1325496d26cffeabb1989e635ef60b1583370
Claire Johnson <c.eliseee@gmail.com>
2017-10-26 11:21:35 -0700

    (refactor) Fix db methods

--- db/purchases/index.js
+++ db/purchases/index.js
@@ -59,77 +59,60 @@ const heapInsertProducts = (array) => {
 };
 
 const getAllUsers = () => (
-  return db.any('SELECT * FROM users')
+  db.any('SELECT * FROM users')
 );
 
 const getAllProducts = () => (
-  return db.any('SELECT * FROM products')
+  db.any('SELECT * FROM products')
 );
 
 const getAllCategories = () => (
-  return db.any('SELECT * FROM categories')
+  db.any('SELECT * FROM categories')
 );
 
-// const getAllPurchases = () => (
-//   pool.query('SELECT * FROM purchase')
-// );
 const getAllPurchases = () => {
   const query = new QueryStream('SELECT * FROM purchase');
   const stream = pool.query(query);
   stream.pipe(JSONStream.stringify()).pipe(process.stdout);
 };
 
-// var pg = require('pg')
-// var QueryStream = require('pg-query-stream')
-// var JSONStream = require('JSONStream')
-
-//pipe 1,000,000 rows to stdout without blowing up your memory usage
-// pg.connect(function(err, client, done) {
-//   if(err) throw err;
-//   var query = new QueryStream('SELECT * FROM generate_series(0, $1) num', [1000000])
-//   var stream = client.query(query)
-//   //release the client when the stream is finished
-//   stream.on('end', done)
-//   stream.pipe(JSONStream.stringify()).pipe(process.stdout)
-// })
-
 const addUser = user => (
-  return db.none(`INSERT INTO users (user_id) VALUES ($/user/) ON CONFLICT (user_id) DO NOTHING`, {user})
+  db.none(`INSERT INTO users (user_id) VALUES ($/user/) ON CONFLICT (user_id) DO NOTHING`, {user})
 );
 
 const addProduct = (product, category) => (
-  return db.none(`INSERT INTO products (product_id, category) VALUES ($/product/, $/category/) ON CONFLICT (product_id) DO NOTHING`, {product, category})
+  db.none(`INSERT INTO products (product_id, category) VALUES ($/product/, $/category/) ON CONFLICT (product_id) DO NOTHING`, {product, category})
 );
 
 const addCategory = category => (
-  return db.none(`INSERT INTO categories (category_id) VALUES ($/category/) ON CONFLICT (category_id) DO NOTHING`, {category})
+  db.none(`INSERT INTO categories (category_id) VALUES ($/category/) ON CONFLICT (category_id) DO NOTHING`, {category})
 );
 
 // NOTE: This function overwrites duplicate purchases so only the most recent
 // purchase and rating are stored
 const addPurchase = (ind, product, user, rating) => (
-  return db.tx(t => t.batch([
-      t.none(`UPDATE purchase SET rating=$/rating/ WHERE user_id=$/user/ AND product_id=$/product/`, {product, user, rating}),
-      t.none(`INSERT INTO purchase (product_id, user_id, rating)
-      SELECT $/product/, $/user/, $/rating/
-      WHERE NOT EXISTS (SELECT 1 FROM purchase WHERE user_id=$/user/ AND product_id=$/product/)
-      `, {user, product, rating})
+  db.tx(t => t.batch([
+    t.none('UPDATE purchase SET rating=$/rating/ WHERE user_id=$/user/ AND product_id=$/product/', { product, user, rating }),
+    t.none(`INSERT INTO purchase (product_id, user_id, rating)
+    SELECT $/product/, $/user/, $/rating/
+    WHERE NOT EXISTS (SELECT 1 FROM purchase WHERE user_id=$/user/ AND product_id=$/product/)
+    `, { user, product, rating }),
   ]))
 );
 
 const deleteAll = () => (
-  return db.tx(t => t.batch([
-      t.none('DELETE FROM purchase'),
-      t.none('DELETE FROM products'),
-      t.none('DELETE FROM users')
+  db.tx(t => t.batch([
+    t.none('DELETE FROM purchase'),
+    t.none('DELETE FROM products'),
+    t.none('DELETE FROM users'),
   ]))
 );
 
 const indexAll = () => (
-  return db.tx(t => t.batch([
-      t.none('CREATE INDEX category_idx ON products (category)'),
-      t.none('CREATE INDEX user_idx ON purchase (user_id)'),
-      t.none('CREATE INDEX product_idx ON purchase (product_id)')
+  db.tx(t => t.batch([
+    t.none('CREATE INDEX category_idx ON products (category)'),
+    t.none('CREATE INDEX user_idx ON purchase (user_id)'),
+    t.none('CREATE INDEX product_idx ON purchase (product_id)'),
   ]))
 );
 
